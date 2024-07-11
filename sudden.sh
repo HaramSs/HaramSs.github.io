16db23363ce72648334a8a722e4c50d36b75f806
HaramSs <haram8274@gmail.com>
2024-07-11 11:06:34 +0900

    add log

--- sudden.sh
+++ sudden.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+
 FILE=~/tmp/_SUDDEN
 
 while true
 do
 	if [[ -f "$FILE" ]]; then
+	    #echo "check $FILE"
 	    echo "SUDDEN!!!"
 	else 
 		clear
 		date
 	fi
     	sleep 3
+	firglet KIA	
 	
 done
